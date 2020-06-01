0c8e37dd2efc99ab003f58d0c28b9be79af7b2fa
jason <jason@evilpaws.org>
2020-06-01 14:17:23 -0500

    db seeds for users. start setting up capistrano. add custom emoji (except custom images) and aliases to emoji_filter.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# user_id: 1
+User.create(display_name: 'shadylady',
+            slack_user:   'U416WGZ8V',
+            color:        '#6699ff'
+)
+# user_id: 2
+User.create(display_name: 'catnik',
+            slack_user:   'U3URFR87N',
+            color:        '#bb88bb'
+)
+# user_id: 3
+User.create(display_name: 'rae',
+            slack_user:   'U3RN8V07N',
+            color:        '#800000'
+           )
+# user_id: 4
+User.create(display_name: 'earldebuke',
+            slack_user:   'U41Q95CSJ',
+            color:        '#222298'
+)
+# user_id: 5
+User.create(display_name: 'robinrahne',
+            slack_user:   'U49KHFJCR',
+            color:        '#cd0d0f'
+)

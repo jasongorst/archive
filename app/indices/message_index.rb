37783a8f0b47587bc18288fcd9d6b2ebfcd4e9ca
Jason Gorst <jgorst@hawk.iit.edu>
2021-01-01 19:43:33 -0600

    remove old index fields. update comment in message model to note that time in in local **server** time.

--- app/indices/message_index.rb
+++ app/indices/message_index.rb
@@ -1,7 +1,5 @@
 ThinkingSphinx::Index.define :message, :with => :real_time do
   indexes text
-  # indexes user.display_name as: :name, sortable: true
-  # indexes channel.name as: :channel, sortable: true
 
   has user_id, type: :integer
   has channel_id, type: :integer

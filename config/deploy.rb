1df53e2b5dfac14f40f01bb772b0c6ac96e827e5
Jason Gorst <jason.gorst@me.com>
2021-06-19 16:13:52 -0500

    add config/en.pak to linked files. set bundle_jobs to 2 for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,10 @@ set :deploy_user, "deploy"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/master.key", "config/en.pak"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "dir", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "dir", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", ".bundle"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

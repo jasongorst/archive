9e76a223a77003fb31803fedfeb0081b08474d6e
Jason Gorst <jason.gorst@me.com>
2021-08-05 22:06:53 -0500

    remove development env check from AdminUser.create! in db/seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com',
+                  password: 'password', password_confirmation: 'password')

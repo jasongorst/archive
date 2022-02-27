c46086a2fdb96cb8bab7f4b18c8bd9586f031cfa
Jason Gorst <jason.gorst@me.com>
2022-02-26 18:29:26 -0600

    rename main branch to main.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "archive"
 set :repo_url, "git@gitlab.com:jasongorst/archive.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/archive"

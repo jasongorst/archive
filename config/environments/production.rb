57ea79b46923ddb0478d5a9b9b3709ae76146951
Jason Gorst <jason.gorst@me.com>
2022-03-02 22:36:55 -0600

    style sign in forms. fix admin_users routes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # set default url options
-  config.action_mailer.default_url_options = { host: 'evilpaws.org' }
+  config.action_mailer.default_url_options = { host: 'archive.evilpaws.org' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources :admin_users, controller: "clearance/users", only: [:create] do
     resource :password,
       controller: "clearance/passwords",
       only: [:edit, :update]

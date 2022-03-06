ce346514033a001532ba7d92b59507e10ff82f81
Jason Gorst <jason.gorst@me.com>
2022-03-06 16:26:33 -0600

    remove redundant to_s in display_controller#show_by_date.

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -15,7 +15,7 @@ class DisplayController < ApplicationController
   def show_by_date
     @channel = Channel.friendly.find(params[:channel_id])
     @date = params[:date].to_date
-    ids = Rails.cache.fetch("messages_in_channel_#{@channel.id}_on_#{@date.to_s}") do
+    ids = Rails.cache.fetch("messages_in_channel_#{@channel.id}_on_#{@date}") do
       @channel.messages.where(posted_on: @date).pluck(:id)
     end
     @messages = Message.where(id: ids).order(posted_at: :asc).page(params[:page])

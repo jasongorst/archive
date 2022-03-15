1d00d8d267bfd71b67a5ac5bec5b2ee012d9d25e
Jason Gorst <jason.gorst@me.com>
2022-03-14 21:47:47 -0500

    add fa-caret-right in application.js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,9 +10,13 @@ ActiveStorage.start()
 import "bootstrap"
 
 // fontawesome
-import { library, icon, dom } from "@fortawesome/fontawesome-svg-core"
-import { faArchive, faExternalLinkAlt, faCalendarAlt } from "@fortawesome/free-solid-svg-icons"
+import { library, dom } from "@fortawesome/fontawesome-svg-core"
+import { faArchive,
+         faExternalLinkAlt,
+         faCalendarAlt,
+         faCaretRight } from "@fortawesome/free-solid-svg-icons"
 library.add(faArchive)
 library.add(faExternalLinkAlt)
 library.add(faCalendarAlt)
+library.add(faCaretRight)
 dom.watch()

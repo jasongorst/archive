250316df5f5a7a5bfb2b529527996e39fa6ed2b3
Jason Gorst <jason.gorst@me.com>
2022-10-24 15:21:25 -0500

    remove css import from flatpickr_controller.js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 import * as ActiveStorage from "@rails/activestorage"
-Rails.start()
 ActiveStorage.start()
 
 import "bootstrap"
--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,5 +1,4 @@
 import Flatpickr from 'stimulus-flatpickr'
-import 'flatpickr/dist/flatpickr.css'
 
 // creates a new Stimulus controller by extending stimulus-flatpickr wrapper controller
 export default class extends Flatpickr {}

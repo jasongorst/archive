b7462a68f25531bbbfe2094bd8af637d4c0529f0
Jason Gorst <jason.gorst@me.com>
2022-10-25 15:32:27 -0500

    replace .erb with .haml in tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
+  plugins: [
+      require("daisyui")
+  ],
 }

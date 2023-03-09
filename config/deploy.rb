e9b7a7429846ea8e5c79404a012948f88f4ce20c
Jason Gorst <jason.gorst@me.com>
2023-03-09 16:26:02 -0600

    update lock version in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.17.2"
 
 set :application, "archive"
 set :repo_url, "git@gitlab.com:jasongorst/archive.git"

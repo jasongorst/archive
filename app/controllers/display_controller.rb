60bcbfaa02a2f8b3aaf85c2648b4586a900b95ef
Jason Gorst <jason.gorst@me.com>
2023-05-06 21:01:55 -0500

    cache messages instead of message#ids in DisplayController#by_date.

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -15,9 +15,11 @@ class DisplayController < ApplicationController
   def by_date
     @channel = Channel.friendly.find(params[:channel_id])
     @date = params[:date].to_date
-    ids = Rails.cache.fetch("messages_in_channel_#{@channel.id}_on_#{@date}") do
-      @channel.messages.where(posted_on: @date).pluck(:id)
+    @messages = Rails.cache.fetch("messages_in_channel_#{@channel.id}_on_#{@date}") do
+      @channel.messages.where(posted_on: @date).order(posted_at: :asc)
     end
-    @messages = Message.where(id: ids).order(posted_at: :asc).page(params[:page])
+
+    # paginate messages
+    @messages = @messages.page(params[:page])
   end
 end

4d248479c8304f37f8aaf91c9f1a76a92cd6c453
Jason Gorst <jason.gorst@me.com>
2023-05-16 22:37:46 -0500

    set ENV["LOG_INFO"] to reduce log level in SlackClient.

--- lib/slack/slack_client.rb
+++ lib/slack/slack_client.rb
@@ -6,7 +6,7 @@ class SlackClient < Slack::Web::Client
       token: Rails.application.credentials.slack_api_token,
       logger: Logger.new(
         STDOUT,
-        level: if Rails.env.development? or ENV["DEBUG"]
+        level: if Rails.env.development? || ENV["LOG_INFO"]
                 Logger::INFO
               else
                 Logger::WARN

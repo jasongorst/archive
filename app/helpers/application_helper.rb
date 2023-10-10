879166b16098d048d1baa0435d8011fab650125b
Jason Gorst <jason.gorst@me.com>
2023-10-09 23:18:57 -0500

    fix active nav highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def channels_with_messages
     Channel.joins(:messages).distinct.order(name: :asc)
   end
+
+  def channel_page?
+    params[:channel_id]
+  end
+
+  def current_channel
+    params[:channel_id]
+  end
 end

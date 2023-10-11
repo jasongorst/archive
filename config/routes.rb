e95874f9e8539c5ca21833058ede53ab927cb845
Jason Gorst <jason.gorst@me.com>
2023-10-11 15:57:48 -0500

    prefer double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  root 'display#index'
+  root "display#index"
 
-  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-  resource :session, controller: 'clearance/sessions', only: [:create]
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :admin_users, controller: 'clearance/users', only: [:create] do
+  resources :admin_users, controller: "clearance/users", only: [:create] do
     resource :password,
-      controller: 'clearance/passwords',
+      controller: "clearance/passwords",
       only: [:edit, :update]
   end
 
-  get '/sign_in', to: 'clearance/sessions#new', as: 'sign_in'
-  delete '/sign_out', to: 'clearance/sessions#destroy', as: 'sign_out'
-  get '/sign_up', to: 'clearance/users#new', as: 'sign_up'
+  get "/sign_in", to: "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out", to: "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up", to: "clearance/users#new", as: "sign_up"
 
   namespace :admin do
     resources :users
@@ -21,10 +21,11 @@ Rails.application.routes.draw do
     resources :attachments
     resources :admin_users
 
-    root to: 'users#index'
+    root to: "users#index"
   end
 
-  get 'search', to: 'search#index'
-  get '/:channel_id', to: 'display#show', as: :channel
-  get '/:channel_id/:date', to: 'display#by_date', as: :channel_date
+  get "search", to: "search#index"
+
+  get ":channel_id", to: "display#show", as: :channel
+  get ":channel_id/:date", to: "display#by_date", as: :channel_date
 end

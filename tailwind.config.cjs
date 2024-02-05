2bb4cfa560f423fa9cc000dbd8dbc08d2e4b252c
Jason Gorst <jason.gorst@me.com>
2024-02-04 19:00:16 -0600

    fix vite cjs deprecation, use daisyui 3 colors for dark theme, adjust navbar active and hover colors

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  plugins: [
+    require("daisyui")
+  ],
+  darkMode: "media",
+  daisyui: {
+    themes: [
+      "winter",
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#661AE6",
+          "primary-content": "#ffffff",
+          secondary: "#D926AA",
+          accent: "#1FB2A5",
+          "accent-content": "#ffffff"
+        }
+      }
+    ],
+    log: false
+  }
+}

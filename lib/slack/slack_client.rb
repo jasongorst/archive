85eabd3f3c719cdd6188fd324e497c2b43b036a4
Jason Gorst <jason.gorst@me.com>
2024-09-17 23:29:39 -0500

    use team token in SlackClient, if it exists, or legacy api token

--- lib/slack/slack_client.rb
+++ lib/slack/slack_client.rb
@@ -2,8 +2,19 @@ require 'slack-ruby-client'
 
 class SlackClient < Slack::Web::Client
   def initialize
+    # use Firnost & Friends team token, if it exists, or fall back to the api token
+
+    firnost_and_friends = Team.find_by_name("Firnost & Friends")
+
+    token = if firnost_and_friends
+              firnost_and_friends.token
+            else
+              Rails.application.credentials.slack_api_token
+            end
+
     config = {
-      token: Rails.application.credentials.slack_api_token,
+      # use Firnost & Friends team token, if it exists, or fall back to the api token
+      token: token,
       logger: Logger.new(
         STDOUT,
         level: if Rails.env.development? || ENV["LOG_INFO"]

e0f5d7538359618fdf4aaf9bf6be9f9396dcaf37
Jason Gorst <jason.gorst@me.com>
2024-09-17 23:04:10 -0500

    add create_accounts.rb script

---
+++ script/create_accounts.rb
@@ -0,0 +1,37 @@
+Account.create!([
+                  {
+                    # shadylady
+                    email: "",
+                    password: "kiln.barfly.crewcut.wren",
+                    admin: false,
+                    user: User.find_by_display_name("shadylady")
+                  },
+                  {
+                    # catnik
+                    email: "",
+                    password: "jumper.palisade.baldric.lottery",
+                    admin: false,
+                    user: User.find_by_display_name("catnik")
+                  },
+                  {
+                    # earldebuke
+                    email: "",
+                    password: "unlucky.late.witless.flung",
+                    admin: false,
+                    user: User.find_by_display_name("earldebuke")
+                  },
+                  {
+                    # robinrahne
+                    email: "",
+                    password: "vinyl.maiden.leaden.bury",
+                    admin: false,
+                    user: User.find_by_display_name("robinrahne")
+                  },
+                  {
+                    # rae
+                    email: "",
+                    password: "huntress.headwind.lock.lighten",
+                    admin: true,
+                    user: User.find_by_display_name("rae")
+                  }
+                ])

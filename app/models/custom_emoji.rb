2acea4b7669732d0c472710bb517b918f97a13dd
Jason Gorst <jason.gorst@me.com>
2024-09-19 23:25:53 -0500

    preprocess CustomEmoji emoji attachment variant

--- app/models/custom_emoji.rb
+++ app/models/custom_emoji.rb
@@ -2,5 +2,7 @@ class CustomEmoji < ApplicationRecord
   validates :name, :url, presence: true
   validates :name, uniqueness: true
 
-  has_one_attached :emoji
+  has_one_attached :emoji do |attachable|
+    attachable.variant :emoji, resize_and_pad: [16, 16, alpha: true], preprocessed: true
+  end
 end

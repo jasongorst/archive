c563eeedf21989047022c6c3146c6fae39175d95
Jason Gorst <jason.gorst@me.com>
2024-09-25 18:52:29 -0500

    move oauth_url into application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,26 @@ module ApplicationHelper
     current_user
   end
 
+  def oauth_url
+    URI::HTTPS.build(
+      host: "slack.com",
+      path: case SlackRubyBotServer::Config.oauth_version
+            when :v2
+              "/oauth/v2/authorize"
+            when :v1
+              "/oauth/authorize"
+            else
+              raise ArgumentError, "Invalid SlackRubyBotServer::Config.oauth_version, must be one of :v1 or :v2."
+            end,
+      query: {
+        scope: SlackRubyBotServer::Config.oauth_scope&.join(","),
+        user_scope: BotServer::USER_OAUTH_SCOPE.join(","),
+        client_id: Rails.application.credentials.slack_client_id,
+        redirect_url: BotServer::REDIRECT_URL
+      }.to_query
+    )
+  end
+
   def long_date(date)
     "#{date.strftime('%B')} #{date.mday.ordinalize}"
   end

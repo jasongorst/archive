a4351597da9b2acb5f9e64bc438a7dfadd6485dd
Jason Gorst <jason.gorst@me.com>
2024-09-30 13:53:10 -0500

    add create_emoji, self.create_all to CustomEmoji

--- app/models/custom_emoji.rb
+++ app/models/custom_emoji.rb
@@ -5,4 +5,24 @@ class CustomEmoji < ApplicationRecord
   has_one_attached :emoji do |attachable|
     attachable.variant :emoji, resize_and_pad: [16, 16, alpha: true], preprocessed: true
   end
+
+  def self.create_all
+    all.each do |custom_emoji|
+      custom_emoji.create_emoji
+    end
+  end
+
+  def image_path
+    Rails.application.routes.url_helpers
+         .rails_blob_path(
+           emoji.variant(:emoji).processed,
+           only_path: true
+         )
+  end
+
+  def create_emoji
+    Emoji.create(name) do |char|
+      char.image_filename = image_path
+    end
+  end
 end

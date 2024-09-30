553bce4633b0da8d29ab5654e804f89c9f75e6b3
Jason Gorst <jason.gorst@me.com>
2024-09-30 13:53:32 -0500

    add create_emoji_alias, self.create_all to EmojiAlias

--- app/models/emoji_alias.rb
+++ app/models/emoji_alias.rb
@@ -1,4 +1,18 @@
 class EmojiAlias < ApplicationRecord
   validates :name, :alias_for, presence: true
   validates :name, uniqueness: true
+
+  def self.create_all
+    # ensure all CustomEmoji have been added to Emoji
+    CustomEmoji.create_all
+
+    all.each do |emoji_alias|
+      emoji_alias.create_emoji_alias
+    end
+  end
+
+  def create_emoji_alias
+    emoji = Emoji.find_by_alias(alias_for)
+    Emoji.edit_emoji(emoji) { |char| char.add_alias name } if emoji
+  end
 end

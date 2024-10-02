ee201dfe73188dbb9e46229baeace406ac92ea1e
Jason Gorst <jason.gorst@me.com>
2024-10-02 14:36:43 -0500

    remove db/seeds.rb

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
-Account.create! email: 'admin@example.com',
-                password: 'a_not_very_secret_password',
-                admin: true

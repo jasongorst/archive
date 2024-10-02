ff9faa90f000a4daaa2755eac3f830a266f9182f
Jason Gorst <jason.gorst@me.com>
2024-10-02 12:47:09 -0500

    remove TeamsController and BotUsersController

--- app/controllers/bot_users_controller.rb
+++
@@ -1,6 +0,0 @@
-class BotUsersController < ApplicationController
-  def index
-    @team = Team.find_by_team_id(params[:team])
-    @bot_users = @team.bot_users
-  end
-end
--- app/controllers/teams_controller.rb
+++
@@ -1,9 +0,0 @@
-class TeamsController < ApplicationController
-  def index
-    @teams = Team.active
-  end
-
-  def show
-    @team = Team.find(params[:id])
-  end
-end

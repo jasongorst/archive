2aaa7f848ec8c4d9e90d8f309a96775c1785286e
Jason Gorst <jason.gorst@me.com>
2024-10-05 01:13:32 -0500

    use solid_queue puma plugin (in development)

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# solid_queue plugin
+plugin :solid_queue

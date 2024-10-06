021014f7080ea7b59c4a72765f99d2ce31233e55
Jason Gorst <jason.gorst@me.com>
2024-10-06 18:34:10 -0500

    remove cruft from bot_user.rb
    (cherry picked from commit 2e2454ca47c1306739cec4236e5977f597686239)

--- app/models/bot_user.rb
+++ app/models/bot_user.rb
@@ -3,23 +3,4 @@ class BotUser < ApplicationRecord
 
   belongs_to :team
   has_one :account, required: false, dependent: :nullify
-
-  scope :active, -> { where(active: true) }
-
-  def deactivate!
-    update!(active: false)
-  end
-
-  def activate!(token)
-    update!(active: true, user_access_token: token)
-  end
-
-  def to_s
-    {
-      display_name: display_name,
-      slack_user: slack_user
-    }.map do |k, v|
-      "#{k}=#{v}" if v
-    end.compact.join(', ')
-  end
 end

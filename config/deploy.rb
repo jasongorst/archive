6df3ec9f85c605c29d7753712a8d427b3eefc433
Jason Gorst <jason.gorst@me.com>
2024-10-07 15:14:29 -0500

    Revert "deploy to archivedm"
    This reverts commit 237666d92f71a679944ae4251ab78a7ed2107b8a.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.1"
 
-set :application, "archivedm"
+set :application, "archive"
 set :repo_url, "git@github.com:jasongorst/archive.git"
 
 # Default branch is :main
@@ -9,7 +9,7 @@ set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :branch, "dm"
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/archivedm"
+set :deploy_to, "/var/www/archive"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh

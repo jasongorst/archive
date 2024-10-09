abaaf62bac6c1563f3edbf32c7420f5732497355
Jason Gorst <jason.gorst@me.com>
2024-10-09 12:59:54 -0500

    remove old scripts

--- script/create_roller-v2_user.rb
+++
@@ -1,5 +0,0 @@
-puts "Creating RollerBot 2 user..."
-
-User.create!(slack_user: "B07J84MNYLW", display_name: "RollerBot 2")
-
-puts "Created."
--- script/fetch_slack_emoji.rb
+++
@@ -1,8 +0,0 @@
-require "slack/emoji"
-
-begin
-  connection = Slack::Emoji.new
-
-  connection.update_emoji_aliases
-  connection.update_custom_emoji
-end

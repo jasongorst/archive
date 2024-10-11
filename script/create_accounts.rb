1f23fd286aca67af3322669e2feb61322143b43b
Jason Gorst <jason.gorst@me.com>
2024-10-10 21:39:08 -0500

    last account

--- script/create_accounts.rb
+++ script/create_accounts.rb
@@ -1,25 +1,25 @@
 accounts = [
-  # {
-  #   # shadylady
-  #   email: "darqueone@hotmail.com",
-  #   password: "kiln.barfly.crewcut.wren",
-  #   admin: false,
-  #   user: User.find_by_display_name("shadylady")
-  # },
-  # {
-  #   # catnik
-  #   email: "catnik@gmail.com",
-  #   password: "jumper.palisade.baldric.lottery",
-  #   admin: false,
-  #   user: User.find_by_display_name("catnik")
-  # },
-  # {
-  #   # earldebuke
-  #   email: "dramavictim@gmail.com",
-  #   password: "unlucky.late.witless.flung",
-  #   admin: false,
-  #   user: User.find_by_display_name("earldebuke")
-  # },
+  {
+    # shadylady
+    email: "darqueone@hotmail.com",
+    password: "kiln.barfly.crewcut.wren",
+    admin: false,
+    user: User.find_by_display_name("shadylady")
+  },
+  {
+    # catnik
+    email: "catnik@gmail.com",
+    password: "jumper.palisade.baldric.lottery",
+    admin: false,
+    user: User.find_by_display_name("catnik")
+  },
+  {
+    # earldebuke
+    email: "dramavictim@gmail.com",
+    password: "unlucky.late.witless.flung",
+    admin: false,
+    user: User.find_by_display_name("earldebuke")
+  },
   {
     # robinrahne
     email: "robinrahne@gmail.com",
@@ -27,17 +27,17 @@ accounts = [
     admin: false,
     user: User.find_by_display_name("robinrahne")
   },
-  # {
-  #   # rae
-  #   email: "rae@evilpaws.org",
-  #   password: "huntress.headwind.lock.lighten",
-  #   admin: true,
-  #   user: User.find_by_display_name("rae")
-  # }
+  {
+    # rae
+    email: "rae@evilpaws.org",
+    password: "huntress.headwind.lock.lighten",
+    admin: true,
+    user: User.find_by_display_name("rae")
+  }
 ]
 
 accounts.each do |account|
-  if Account.find_or_create_by(email: account[:email])
+  if Account.find_by_email(account[:email])
     puts "#{account[:email]} already exists."
   else
     a = Account.create!(account)

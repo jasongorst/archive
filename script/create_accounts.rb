db975ad295bc8403d98b15f0a6730bdbaf03026b
Jason Gorst <jason.gorst@me.com>
2024-10-10 19:21:45 -0500

    add final account

--- script/create_accounts.rb
+++ script/create_accounts.rb
@@ -1,43 +1,43 @@
 accounts = [
-  {
-    # shadylady
-    email: "darqueone@hotmail.com",
-    password: "kiln.barfly.crewcut.wren",
-    admin: false,
-    user: User.find_by_display_name("shadylady")
-  },
-  {
-    # catnik
-    email: "catnik@gmail.com",
-    password: "jumper.palisade.baldric.lottery",
-    admin: false,
-    user: User.find_by_display_name("catnik")
-  },
-  {
-    # earldebuke
-    email: "dramavictim@gmail.com",
-    password: "unlucky.late.witless.flung",
-    admin: false,
-    user: User.find_by_display_name("earldebuke")
-  },
   # {
-  #   # robinrahne
-  #   email: "",
-  #   password: "vinyl.maiden.leaden.bury",
+  #   # shadylady
+  #   email: "darqueone@hotmail.com",
+  #   password: "kiln.barfly.crewcut.wren",
   #   admin: false,
-  #   user: User.find_by_display_name("robinrahne")
+  #   user: User.find_by_display_name("shadylady")
+  # },
+  # {
+  #   # catnik
+  #   email: "catnik@gmail.com",
+  #   password: "jumper.palisade.baldric.lottery",
+  #   admin: false,
+  #   user: User.find_by_display_name("catnik")
+  # },
+  # {
+  #   # earldebuke
+  #   email: "dramavictim@gmail.com",
+  #   password: "unlucky.late.witless.flung",
+  #   admin: false,
+  #   user: User.find_by_display_name("earldebuke")
   # },
   {
-    # rae
-    email: "rae@evilpaws.org",
-    password: "huntress.headwind.lock.lighten",
-    admin: true,
-    user: User.find_by_display_name("rae")
-  }
+    # robinrahne
+    email: "robinrahne@gmail.com",
+    password: "vinyl.maiden.leaden.bury",
+    admin: false,
+    user: User.find_by_display_name("robinrahne")
+  },
+  # {
+  #   # rae
+  #   email: "rae@evilpaws.org",
+  #   password: "huntress.headwind.lock.lighten",
+  #   admin: true,
+  #   user: User.find_by_display_name("rae")
+  # }
 ]
 
 accounts.each do |account|
-  if Account.find_by_email(account[:email])
+  if Account.find_or_create_by(email: account[:email])
     puts "#{account[:email]} already exists."
   else
     a = Account.create!(account)

a92a47491823c79d90703fcbfcb832fb2bb33394
Jason Gorst <jason.gorst@me.com>
2024-10-30 15:12:55 -0500

    add job to reparse messages with emoji

---
+++ app/jobs/reparse_messages_job.rb
@@ -0,0 +1,16 @@
+class ReparseMessagesJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    Message.all.each do |message|
+      verbatim = Hashie::Mash.new(JSON.parse(message.verbatim))
+      next unless /:[\w+-_]+?:/ =~ verbatim.text
+
+      slack_message = Slack::Message.new(verbatim)
+      next if slack_message.user.nil?
+
+      message.text = slack_message.text
+      message.save!
+    end
+  end
+end

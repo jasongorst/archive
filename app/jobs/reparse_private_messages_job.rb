db7892031e2e7b51f4d27b89e3d5ad6ee714341f
Jason Gorst <jason.gorst@me.com>
2024-10-31 12:18:13 -0500

    add job to reparse private messages with emoji

---
+++ app/jobs/reparse_private_messages_job.rb
@@ -0,0 +1,16 @@
+class ReparsePrivateMessagesJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    PrivateMessage.all.each do |private_message|
+      verbatim = Hashie::Mash.new(JSON.parse(private_message.verbatim))
+      next unless /:[\w+-_]+?:/ =~ verbatim.text
+
+      slack_message = Slack::Message.new(verbatim)
+      next if slack_message.user.nil?
+
+      private_message.text = slack_message.text
+      private_message.save!
+    end
+  end
+end
